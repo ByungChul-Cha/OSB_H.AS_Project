3a92cc216e187c3c7acb5be55186467bed72037d
ByungChul <lastsc1404@gmail.com>
2024-04-28 20:39:47 +0900

    앱 기능 추가
    사용자 정보
    홈, 식별검색, 카메라로 검색, 커뮤니티, 환경설정 창 추가

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'H.AS app',
+      title: 'H.AS App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
@@ -21,15 +21,85 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('H.AS Medi App'),
+        title: const Text('H.AS Medi App'),
         //앱 상단에 뜨는 이름
-        leading: Icon(Icons.menu, color: Colors.white),
-        //메뉴 버튼의 아이콘, 색깔 설정
-        titleTextStyle: TextStyle(color: Colors.white),
+        centerTitle: true,
+        //이름을 중앙에 배치
+        elevation: 0.0,
+        //앱 바의 그림자 효과를 없앰
+        titleTextStyle: const TextStyle(color: Colors.white),
         //타이틀의 색깔 설정
         backgroundColor: Colors.blue,
         //앱 바의 배경 색 설정
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/image/images.jpg'),
+              ),
+              accountName: Text("테스트용"),
+              accountEmail: Text('test@test.com'),
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+              ),
+            ),
+            //유저 정보 그려주는 코드
+
+            ListTile(
+              leading: const Icon(Icons.home),
+              iconColor: Colors.black,
+              focusColor: Colors.black,
+              title: const Text('홈'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            //메뉴에 홈 창을 만듦
+
+            ListTile(
+              leading: const Icon(Icons.search),
+              iconColor: Colors.black,
+              focusColor: Colors.black,
+              title: const Text('식별 검색'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            //메뉴에 식별 검색 창을 만듦
+
+            ListTile(
+              leading: const Icon(Icons.photo_camera),
+              iconColor: Colors.black,
+              focusColor: Colors.black,
+              title: const Text('카메라로 검색'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            //메뉴에 카메라로 검색 창을 만듦
+
+            ListTile(
+              leading: const Icon(Icons.chat),
+              iconColor: Colors.black,
+              focusColor: Colors.black,
+              title: const Text('커뮤니티'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            //메뉴에  커뮤니티 창을 만듦
+
+            ListTile(
+              leading: const Icon(Icons.settings),
+              iconColor: Colors.black,
+              focusColor: Colors.black,
+              title: const Text('환경설정'),
+              onTap: () {},
+              trailing: const Icon(Icons.navigate_next),
+            ),
+            //메뉴에 환경설정 창을 만듦
+          ],
+        ),
+      ),
+      //메뉴 버튼을 만듦
     );
   }
 }
